c36112d549f8904f508a4d9301f88479750b23b1
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2019-12-03 22:42:05 -0800

    got search to work

--- my-github-usercards/src/App.js
+++ my-github-usercards/src/App.js
@@ -41,16 +41,61 @@ class App extends React.Component {
     })
     }
 
+    // componentDidUpdate(prevProps, prevState){
+    //   if(prevState.user !== this.state.user){
+    //     axios
+    //     .get(`https://api.github.com/users/${this.state.newUser}`)
+    //     .then(response => {
+    //       this.setState({})
+    //     })
+    //   }
+    // }
 
+    handleChanges = event => {
+      this.setState({
+        newUser: event.target.value
+      });
+    };
+
+    searchNewUser = event => {
+      event.preventDefault();
+      axios
+        .get(`https://api.github.com/users/${this.state.newUser}`)
+        .then(response => {
+          this.setState({user: {
+            name: response.data.name,
+            handle: response.data.login,
+            link: response.data.html_url,
+            followers: response.data.followers,
+            location: response.data.location,
+            bio: response.data.bio,
+            avatar: response.data.avatar_url}
+        });
+      });
+      
+      axios
+    .get(`https://api.github.com/users/${this.state.newUser}/followers`)
+    .then(response =>{
+      console.log(response.data);
+      this.setState({ followers: response.data})
+    })
+    };
 
   render() {
     return (
       <div className="App">
         <h1>GitHub User Cards</h1>
+        <input
+          type="text"
+          value={this.state.newUser}
+          onChange={this.handleChanges}
+          />
+          <button onClick={this.searchNewUser}>Find User</button>
         <Container>
           <Row>
           <Col xs="12" s="6" md="4">
             <Card>
+              <h1>Your All Star Coder!</h1>
               <CardImg top width="100%" src={this.state.user.avatar} alt="avatar photo" />
                   <CardBody>
                     <CardTitle className="title">{this.state.user.name}</CardTitle>
@@ -64,7 +109,7 @@ class App extends React.Component {
             </Col>
           </Row>
       </Container>
-        
+        <h2>My Followers</h2>
         <FollowerList followers={this.state.followers}/>
       </div>
     );
